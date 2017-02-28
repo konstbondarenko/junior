b72a0b8ccd6920d9ee4b5cc2940861289857a9ac
Konstantin Bondarenko <1kbkbkbkb1@gmail.com>
2017-02-28 23:32:54 +0300

    Added CalculateTest.java

---
+++ chapter_001/src/test/java/ru/kbond/CalculateTest.java
@@ -0,0 +1,35 @@
+package ru.kbond;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author kbondarenko
+ * @since 28.02.2017
+ * @version 1
+ */
+public class CalculateTest {
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenAddOneToOneThenTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(
+                out.toString(),
+                is(
+                        String.format(
+                                "Hello World%s",
+                                System.getProperty("line.separator")
+                        )
+                )
+        );
+    }
+}
