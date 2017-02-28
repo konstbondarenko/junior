22d34496f2d2784b3504a7ed6c64a4b3046d74bf
Konstantin Bondarenko <1kbkbkbkb1@gmail.com>
2017-02-28 13:08:42 +0300

    modified Hello World

---
+++ chapter_001/src/main/java/ru/kbond/Calculate.java
@@ -0,0 +1,20 @@
+package ru.kbond;
+
+/**
+ * Class Calculate решение задачи части 001 урок 1.
+ * @author bondarenko
+ * @since 28.02.2017
+ * @version 1
+*/
+
+public class Calculate {
+	/**
+	 * main.
+	 * @param args - args.
+	 * @return String[] args.
+	*/
+	
+	public static void main(String[] args) {		
+		System.out.println("Hello world.");
+	}
+}
--- src/main/java/ru/kbond/Calculate.java
+++
@@ -1,7 +0,0 @@
-package ru.kbond;
-
-public class Calculate{
-	public static void main(String[] args){		
-		System.out.println("Hello world.");
-	}
-}
