04cc87304fcf19424949631058c575c5720ae961
Konstantin Bondarenko <1kbkbkbkb1@gmail.com>
2017-03-31 00:47:09 +0300

    Added ArrayDuplicate.java

---
+++ chapter_001/src/main/java/ru/kbond/array/ArrayDuplicate.java
@@ -0,0 +1,40 @@
+package ru.kbond.array;
+
+import java.util.Arrays;
+
+/**
+ * Class ArrayDuplicate для удаления дубликата в массиве.
+ * @author kbondarenko
+ * @since 31.03.2017
+ * @version 1
+ */
+public class ArrayDuplicate {
+	/**
+	 * Метод для поворота квадратного массива.
+	 * @param array принимаемый массив.
+	 * @return результат
+	 */
+    public String[] remove(String[] array) {
+        int count = 0;
+        int cut;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 1 + i; j < array.length; j++) {
+                if (array[i] != null && array[i].equals(array[j])) {
+                    count++;
+                    array[j] = null;
+                }
+            }
+        }
+        cut = array.length - count;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1 + i; j < array.length - 1; j++) {
+                if (array[j] == null) {
+                    String tmp = array[j + 1];
+                    array[j + 1] = array[j];
+                    array[j] = tmp;
+                }
+            }
+        }
+    return Arrays.copyOf(array, cut);
+    }
+}
