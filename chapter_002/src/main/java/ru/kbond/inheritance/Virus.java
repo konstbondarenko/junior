78151dbc0ee130952cb5776a754a30b5c7849859
Konstantin Bondarenko <1kbkbkbkb1@gmail.com>
2017-05-09 22:18:21 +0300

    Added Virus.java

---
+++ chapter_002/src/main/java/ru/kbond/inheritance/Virus.java
@@ -0,0 +1,17 @@
+package ru.kbond.inheritance;
+
+/**
+ * Class Virus.
+ * @author kbondarenko
+ * @since 09.05.2017
+ * @version 1
+ */
+public class Virus extends Profession {
+	/**
+	 * Конструктор.
+	 * @param name - имя.
+	 */
+    public Virus(String name) {
+        this.setName(name);
+    }
+}
