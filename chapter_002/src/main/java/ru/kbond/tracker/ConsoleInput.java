9ef025b744878a967253eed0177471a43673a972
Konstantin Bondarenko <1kbkbkbkb1@gmail.com>
2017-05-29 03:25:55 +0300

    Added ConsoleInput.java

---
+++ chapter_002/src/main/java/ru/kbond/tracker/ConsoleInput.java
@@ -0,0 +1,25 @@
+package ru.kbond.tracker;
+
+import java.util.Scanner;
+
+/**
+ * Class ConsoleInput - используется для ввода пользовательских данных из консоли.
+ * @author kbondarenko
+ * @since 29.05.2017
+ * @version 1
+ */
+public class ConsoleInput {
+    /**
+     * Поле scanner.
+     */
+    private Scanner scanner = new Scanner(System.in);
+    /**
+     * Метод принимающий ввод пользователя.
+     * @param question - принимаемая строка.
+     * @return результат.
+     */
+    public String ask(String question) {
+        System.out.println(question);
+        return scanner.nextLine();
+    }
+}
