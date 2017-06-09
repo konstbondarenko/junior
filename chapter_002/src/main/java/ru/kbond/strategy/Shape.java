cfc5eefe4bdd8cd567545c56202648e0cc9c4669
Konstantin Bondarenko <1kbkbkbkb1@gmail.com>
2017-06-09 15:43:41 +0300

    Added Shape.java

---
+++ chapter_002/src/main/java/ru/kbond/strategy/Shape.java
@@ -0,0 +1,15 @@
+package ru.kbond.strategy;
+
+/**
+ * Interface Shape - для создания разных фигур.
+ * @author kbondarenko
+ * @since 09.06.2017
+ * @version 1
+ */
+public interface Shape {
+    /**
+     * Метод реализующий фигуру.
+     * @return - return.
+     */
+    String pic();
+}
