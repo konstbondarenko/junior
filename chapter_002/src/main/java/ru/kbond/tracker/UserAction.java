998a970b79de8fca66f5d11a84b230426767a479
Konstantin Bondarenko <1kbkbkbkb1@gmail.com>
2017-06-22 02:48:57 +0300

    Added UserAction.java

---
+++ chapter_002/src/main/java/ru/kbond/tracker/UserAction.java
@@ -0,0 +1,36 @@
+package ru.kbond.tracker;
+
+import java.util.Date;
+
+/**
+ * Interface UserAction - используется для описания методов общих для всех событий.
+ * @author kbondarenko
+ * @since 22.06.2017
+ * @version 1
+ */
+public interface UserAction {
+    /**
+     * Поле задающее позицию меню.
+     * @return - return.
+     */
+    int key();
+    /**
+     * Метод реализующий взаимодействие с массивом.
+     * @param input - способ ввода.
+     * @param tracker - массив с которым производятся действия.
+     */
+    void execute(Input input, Tracker tracker);
+    /**
+     * Поле с описанием пункта меню.
+     * @return - return.
+     */
+    String info();
+    /**
+     * Поле создающее дату.
+     */
+    Date D = new Date();
+    /**
+     * Поле создающее дату для заявки.
+     */
+    long T = D.getTime();
+}
