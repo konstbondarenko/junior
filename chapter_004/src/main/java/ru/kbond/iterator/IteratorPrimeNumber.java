414c8e2dc1597498bf9594319c1dc02e28c3b3ca
konstbondarenko <1kbkbkbkb1@gmail.com>
2018-02-13 22:43:35 +0300

    Iterator for prime numbers, tests.

---
+++ chapter_004/src/main/java/ru/kbond/iterator/IteratorPrimeNumber.java
@@ -0,0 +1,78 @@
+package ru.kbond.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Class iterator for prime numbers.
+ * @author kbondarenko
+ * @since 13.02.2018
+ * @version 1
+ */
+public class IteratorPrimeNumber implements Iterator<Integer> {
+    /**
+     * The field storing array.
+     * @param value  array value.
+     */
+    private final int[] values;
+    /**
+     * The field prime element index.
+     * @param index  prime element index.
+     */
+    private int index = 0;
+    /**
+     * Constructor.
+     * @param values  array.
+     */
+    public IteratorPrimeNumber(final int[] values) {
+        this.values = values;
+    }
+    /**
+     * Method returns {@code true} if the iteration has more elements.
+     * @return  {@code true} if the iteration has more elements.
+     */
+    @Override
+    public boolean hasNext() {
+        boolean primeHasNext = false;
+        while (index < values.length) {
+            if (primeNumber(values[index])) {
+                primeHasNext = true;
+                break;
+            }
+            index++;
+        }
+        return primeHasNext;
+    }
+    /**
+     * Method returns the next element in the iteration.
+     * @return  the next element in the iteration.
+     * @throws  NoSuchElementException if the iteration has no more elements.
+     */
+    @Override
+    public Integer next() {
+        hasNext();
+        if (index == values.length) {
+            throw new NoSuchElementException();
+        }
+        return values[index++];
+    }
+    /**
+     * Method returns {@code true} if the number is prime.
+     * @return  {@code true} if the number is prime.
+     */
+    private boolean primeNumber(int number) {
+        boolean prime = false;
+        for (int i = 2; i <= number; i++) {
+            if ((i == number) || (i > Math.sqrt(number))) {
+                prime = true;
+                break;
+            }
+            if (number % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+}
+
