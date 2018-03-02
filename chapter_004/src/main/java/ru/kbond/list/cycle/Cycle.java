fdc02826b12d40aeb30cd15636a6595e36fb673d
konstbondarenko <1kbkbkbkb1@gmail.com>
2018-03-02 16:26:55 +0300

    Checking the cycle, tests.

---
+++ chapter_004/src/main/java/ru/kbond/list/cycle/Cycle.java
@@ -0,0 +1,35 @@
+package ru.kbond.list.cycle;
+
+/**
+ * Class determining that the container contains closures.
+ *
+ * @author kbondarenko
+ * @since 27.02.2018
+ * @version 1
+ */
+public class Cycle<E> {
+    /**
+     * Method determining that the container contains closures.
+     * Tortoise and hare pointers to nodes. The hare goes one step
+     * ahead of the turtle until they meet or the end of the list is null.
+     *
+     * @param first  the first node of the container being checked.
+     * @return  {@code true} if there is a cycle or {@code false}
+     *          if there are no cycles.
+     */
+    boolean hasCycle(NodeCycle<E> first) {
+        NodeCycle tortoise = first;
+        NodeCycle hare = first;
+        boolean cycle = false;
+        while (hare != null && hare.next != null) {
+            tortoise = tortoise.next;
+            hare = hare.next.next;
+
+            if (tortoise == hare) {
+                cycle = true;
+                break;
+            }
+        }
+        return cycle;
+    }
+}
