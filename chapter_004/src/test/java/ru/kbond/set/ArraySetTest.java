daa9dc85d6add08ff1474f91b18f61e4d410b770
konstbondarenko <1kbkbkbkb1@gmail.com>
2018-03-02 17:00:04 +0300

    Implementing the Set collection on an array, tests.

---
+++ chapter_004/src/test/java/ru/kbond/set/ArraySetTest.java
@@ -0,0 +1,33 @@
+package ru.kbond.set;
+
+import org.junit.Test;
+import java.util.Iterator;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+/**
+ * Test.
+ * @author kbondarenko
+ * @since 01.03.2018
+ * @version 1
+ */
+public class ArraySetTest {
+    /**
+     * Test.
+     */
+    @Test
+    public void whenAddIntegersThanGetIntegersWithoutDuplicates() {
+        ArraySet<Integer> set = new ArraySet<>();
+        set.add(1);
+        set.add(2);
+        set.add(1);
+        set.add(2);
+        set.add(3);
+
+        Iterator<Integer> it = set.iterator();
+
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(3));
+    }
+}
