e1b055c2cd7f095a8e70a553371ded1c2c4a738b
konstbondarenko <1kbkbkbkb1@gmail.com>
2018-06-22 18:45:21 +0300

    Refactor ParallelSearch.

--- chapter_004/src/main/java/ru/kbond/multithreading/visitor/ParallelSearch.java
+++ chapter_004/src/main/java/ru/kbond/multithreading/visitor/ParallelSearch.java
@@ -1,13 +1,14 @@
 package ru.kbond.multithreading.visitor;
 
-import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
+
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -15,8 +16,8 @@ import java.util.concurrent.LinkedBlockingQueue;
  * directory in files with the specified extension.
  *
  * @author kbondarenko
- * @since 17.06.2018
  * @version 1
+ * @since 17.06.2018
  */
 @ThreadSafe
 public class ParallelSearch {
@@ -24,30 +25,35 @@ public class ParallelSearch {
     private final String text;
     private final List<String> exts;
     /**
+     * A flag indicating the end of the search thread.
+     */
+    private volatile boolean finishSearch = true;
+    /**
      * A flag indicating the end of the reading thread.
      */
-    private volatile boolean finish = true;
+    private volatile boolean finishRead = false;
     /**
      * The queue contains the paths of the found files.
      */
-    @GuardedBy("this")
     private final Queue<String> files = new LinkedBlockingQueue<>();
     /**
      * List contains the file paths with the word found.
      */
-    @GuardedBy("this")
-    private final List<String> paths = new ArrayList<>();
+    private final List<String> paths = new CopyOnWriteArrayList<>();
+
     /**
      * Constructor.
-     * @param root  directories where you need to search for files.
-     * @param text  text you want to find.
-     * @param exts  list of extensions.
+     *
+     * @param root directories where you need to search for files.
+     * @param text text you want to find.
+     * @param exts list of extensions.
      */
     public ParallelSearch(String root, String text, List<String> exts) {
         this.root = root;
         this.text = text;
         this.exts = exts;
     }
+
     /**
      * The method starts the search and read streams.
      */
@@ -61,23 +67,22 @@ public class ParallelSearch {
                 } catch (IOException | InterruptedException e) {
                     e.printStackTrace();
                 }
-                finish = false;
+                finishSearch = false;
             }
         };
         Thread read = new Thread() {
             @Override
             public void run() {
-                while (finish) {
+                while (finishSearch) {
                     while (!files.isEmpty()) {
                         try {
-                            String content = new String(Files.readAllBytes(Paths.get(files.peek())));
-                            String[] sentence = content.split(" ");
-                            for (int i = 0; i < sentence.length; i++) {
-                                if (sentence[i].equals(text)) {
-                                    paths.add(files.poll());
+                            String filesPath = files.poll();
+                            List<String> allLines = Files.readAllLines(
+                                    Paths.get(filesPath), Charset.forName("ISO-8859-1"));
+                            for (String allLine : allLines) {
+                                if (allLine.contains(text)) {
+                                    paths.add(filesPath);
                                     break;
-                                } else if (i == sentence.length - 1) {
-                                    files.remove();
                                 }
                             }
                         } catch (IOException e) {
@@ -85,6 +90,7 @@ public class ParallelSearch {
                         }
                     }
                 }
+                finishRead = false;
             }
         };
         search.start();
@@ -96,13 +102,17 @@ public class ParallelSearch {
             e.printStackTrace();
         }
     }
+
     /**
      * The method displays a list of found file paths matching the query.
      *
-     * @return  list of found file paths matching the query.
+     * @return list of found file paths matching the query.
      */
-    public List<String> result() {
+    public List<String> result() throws InterruptedException {
         synchronized (this) {
+            while (this.finishRead) {
+                this.wait();
+            }
             return this.paths;
         }
     }
@@ -112,12 +122,14 @@ public class ParallelSearch {
      */
     private class MyFileVisitor extends SimpleFileVisitor<Path> {
         private List<String> exts;
+
         /**
          * Constructor.
          */
         public MyFileVisitor(List<String> exts) {
             this.exts = exts;
         }
+
         /**
          * Invoked for a file in a directory.
          */
